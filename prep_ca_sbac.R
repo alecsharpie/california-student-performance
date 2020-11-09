ece13b1feb51b05ed401de3ffb23e99cdaa7f54a
Alec Sharp <alecsharpie@gmail.com>
2020-11-09 22:52:33 +1100

    prepare and save the data

--- prep_ca_sbac.R
+++ prep_ca_sbac.R
@@ -24,20 +24,28 @@ tab_prep_long <- tab_prep %>%
 levels(as.factor(tab_prep$test_name))
 
 
-# add a single column containing correct test names and remove the old two
-tab_prep %>%
+# add a single column containing correct test names, creating conditionally from test_id and test_type columns and then remove them
+tab_prep_clean <- tab_prep_long %>%
   mutate(test_name = case_when(
     test_id == "SB_ELA" & test_type == "pct_met_and_above" ~ "English - Overall",
-    test_id == "SB_ELA" & test_type == "area_1_percentage_above_standard" ~ "English - Overall",
-    test_id == "SB_ELA" & test_type == "area_2_percentage_above_standard" ~ "English - Overall",
-    test_id == "SB_ELA" & test_type == "area_3_percentage_above_standard" ~ "English - Overall",
-    test_id == "SB_ELA" & test_type == "area_4_percentage_above_standard" ~ "English - Overall",
-    x %% 5 == 0 ~ "fizz",
-    x %% 7 == 0 ~ "buzz",
-    TRUE ~ as.character(x)
-  ))
+    test_id == "SB_ELA" & test_type == "area_1_percentage_above_standard" ~ "English - Reading",
+    test_id == "SB_ELA" & test_type == "area_2_percentage_above_standard" ~ "English - Writing",
+    test_id == "SB_ELA" & test_type == "area_3_percentage_above_standard" ~ "English - Listening",
+    test_id == "SB_ELA" & test_type == "area_4_percentage_above_standard" ~ "English - Research/Inquiry",
+    test_id == "SB_MATH" & test_type == "pct_met_and_above" ~ "Math - Overall",
+    test_id == "SB_MATH" & test_type == "area_1_percentage_above_standard" ~ "Math - Concepts/Procedures",
+    test_id == "SB_MATH" & test_type == "area_2_percentage_above_standard" ~ "Math - Problem Solving/Analysis",
+    test_id == "SB_MATH" & test_type == "area_3_percentage_above_standard" ~ "Math - Communicating Reasoning",
+    TRUE ~ "undefined"
+  )) %>%
+  filter(!test_name == "undefined") %>%
+  mutate(test_id = NULL) %>%
+  mutate(test_type = NULL) %>%
+  arrange(group_id)
 
-#subgroup_id and test_type
+# save new data to a csv
+write.csv(tab_prep_clean, "california-student-performance/data/ca_state_data_for_tableau.csv")
+
 
 
   
